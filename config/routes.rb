38283ab27cc0de6edfbf52935cf2a184966dcfd5
OmarMWarraich <earthling986@gmail.com>
2023-09-15 14:47:09 +0500

    feat: Authentication & Registration Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logged_out, to: "sessions#logged_out"
+  get :logged_in, to: "sessions#logged_in"
   root "static#home"
 end
