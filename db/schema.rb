545408c263961275391cc5197a95e430e6d82d2c
OmarMWarraich <earthling986@gmail.com>
2023-09-17 19:51:11 +0500

    feat: Todo Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_15_085302) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_17_144941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_085302) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todos", "users"
 end
